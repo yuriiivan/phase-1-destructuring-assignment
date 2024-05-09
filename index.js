22e8d11f7044163c4286d37d717481e0cbf0fc75
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-09 14:01:06 -0400

    Add array destructuring

--- index.js
+++ index.js
@@ -34,11 +34,11 @@ const [bessie, , dolly, babe, little] = farmAnimals.split(' ')
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
 const [blackAndWhite, , black, pink, ] = farmAnimals.split(' ')
 // Arrays
-
+const [red, orange, yellow, green, blue, indigo, violet] = colors
 // 4. Use destructuring to assign appropriate variables using the color names.
-
+const [r, o, y, g, b, i, v] = colors
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
-
+const [,,,,,indg,] = colors
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
 
 // Objects
